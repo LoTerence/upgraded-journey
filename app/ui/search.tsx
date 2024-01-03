c12ca631adee035e9ffacc390037055ed1af3949
Terence Lo <terencelomoa@gmail.com>
2024-01-03 14:01:20 -0800

    use debounce

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export default function Search({ placeholder }: { placeholder: string }) {
@@ -8,7 +9,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter()
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`)
+    
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
@@ -17,7 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     // update the URL with the user's search data
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
