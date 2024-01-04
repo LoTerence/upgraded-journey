3abcf017727f751eff61860a4e02bcc36de90dab
Terence Lo <terencelomoa@gmail.com>
2024-01-03 17:46:08 -0800

    createInvoice server action

---
+++ app/lib/actions.ts
@@ -0,0 +1,40 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  // use zod to validate the Invoice form data before saving it to a database
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  // insert data into db
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // clear client-side router cache and trigger a new request to the server
+  // aka revalidate the path:
+  revalidatePath('/dashboard/invoices');
+
+  // redirect user back to '/dashboard/invoices' page
+  redirect('/dashboard/invoices');
+}
